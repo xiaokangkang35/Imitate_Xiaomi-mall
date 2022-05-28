ab599ac3318adffba6cce4a36641b486223aa996
康宝 <1969806517@qq.com>
2022-05-28 23:57:05 +0800

    购物车界面
    1.对于产品版块中‘加入购物车’按钮鼠标悬浮样式的调整。

--- js/shop.js
+++ js/shop.js
@@ -1,7 +1,7 @@
 window.onload=function(){
     var before = document.getElementById('re-item');
     var after = document.getElementById('re-ac');
-    before.onmouseover=function(){
+    before.onmouseover=after.onmouseover=function(){
         after.style.opacity='1';
         after.style.transform='translate3d(0,0,0)';
     }
