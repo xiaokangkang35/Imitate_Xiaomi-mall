67cbd010e8099eb91c601b98237ae61e63b84817
康宝 <1969806517@qq.com>
2022-05-30 21:10:22 +0800

    购物车界面主要内容添加
    1.对于购物车界面中 page 内容进行添加。
    2.主界面顶部广告关闭按钮颜色更改。

--- js/shop.js
+++ js/shop.js
@@ -1,13 +1,19 @@
 window.onload=function(){
-    var before = document.getElementById('re-item');
-    var after = document.getElementById('re-ac');
-    before.onmouseover=after.onmouseover=function(){
-        after.style.opacity='1';
-        after.style.transform='translate3d(0,0,0)';
+
+    function shop(a,b){
+        var before = document.getElementById(a);
+        var after = document.getElementById(b);
+        before.onmouseover=after.onmouseover=function(){
+            after.style.opacity='1';
+            after.style.transform='translate3d(0,0,0)';
+        }
+        before.onmouseout=function(){
+            after.style.opacity='0';
+            after.style.transform='translate3d(0,200%,0)';
+        }
     }
-    before.onmouseout=function(){
-        after.style.opacity='0';
-        after.style.transform='translate3d(0,200%,0)';
-    }
+    shop('re-item-1','re-ac-1');
+    shop('re-item-2','re-ac-2');
+    shop('re-item-6','re-ac-6');
 
 }
