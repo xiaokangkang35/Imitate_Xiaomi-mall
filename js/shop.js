233eeedfc00e9e82e109aaf586c76f6fc5b29634
康宝 <1969806517@qq.com>
2022-06-04 18:52:11 +0800

    购物车界面动画问题
    1. 修复购物车界面中加入购物车按钮鼠标移入和移除动画异常问题。

--- js/shop.js
+++ js/shop.js
@@ -7,7 +7,7 @@ window.onload=function(){
             after.style.opacity='1';
             after.style.transform='translate3d(0,0,0)';
         }
-        before.onmouseout=function(){
+        before.onmouseout=after.onmouseout=function(){
             after.style.opacity='0';
             after.style.transform='translate3d(0,200%,0)';
         }
